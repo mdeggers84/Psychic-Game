c263112fab2fc72cd585eaac8b5d25517f4ecc3d
Michael Eggers <mdeggers84@gmail.com>
2017-02-27 10:58:27 -0500

    game template setup

---
+++ assets/javascript/game.js
@@ -0,0 +1,62 @@
+// variables for storing data / updating html file / logging choices
+var gameUpdate = document.getElementById("game");
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var guessArr = [];
+var guessPool = "abcdefghijklmnopqrstuvwxyz";
+var compChoice = randomLetter();
+console.log("start " + compChoice);
+
+document.onkeyup = function(event) {
+	var userGuess = event.key;
+	
+	// initial condition that confirms user is choosing from the pool of possible winnng choices
+	if (guessPool.indexOf(userGuess) !== -1) {
+		// if user is right, they get a point and game is reset
+		if (userGuess === compChoice) {
+			wins += 1;
+			reset();
+			print();
+		// if user is wrong, they lose a guess and have to try again
+	} else {
+		guessesLeft -= 1;
+			// if number of guesses runs out, a loss is logged and game is reset
+			if (guessesLeft === 0) {
+				losses += 1;
+				reset();
+				print();
+			// while guesses remain above 0, guess count is reduced, no win or loss is logged
+		} else {
+			guessArr.push(userGuess);
+			print();
+			compChoice = randomLetter();
+			console.log("loss " + compChoice);
+		}
+	}
+}
+};
+
+// i setup a series of specific functions to make editing/running more versatile and organized
+
+// assigns random letter to var compChoice when called
+function randomLetter() {
+	return guessPool.charAt(Math.floor(Math.random() * guessPool.length));
+}
+
+// resets the game after win or loss
+function reset() {
+	guessesLeft = 9;
+	guessArr = [];
+	compChoice = randomLetter();
+}
+
+// updates the html file with current results
+function print() {
+	var html = "<p>Wins: " + wins + "</p>" +
+	"<p>Losses: "+ losses + "</p>" +
+	"<p>Guesses Left: " + guessesLeft + "</p>" +
+	"<p>Your Guesses: "+ guessArr.join(', ') + "</p>";
+
+	document.querySelector('#game').innerHTML = html;
+}
